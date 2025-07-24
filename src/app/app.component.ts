0446af12cb68d25697493f7987808c0b2eb6b254
Gianluigi <gianluigi.capriello@gmail.com>
2025-07-24 12:40:09 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductGrid } from "./components/products-grid/products-grid.component";
+import { NavbarComponet } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductGrid],
+  imports: [RouterOutlet, ProductGrid, NavbarComponet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fake-store-api';
 }
+ 
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+import { Store } from "@ngrx/store";
+import { selectCart } from "../../state/product/product.selectors";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+@Component({
+    selector: 'navbar',
+    styleUrl: './navbar.componet.css',
+    templateUrl: './navbar.componet.html',
+    imports: [MatBadgeModule, MatIconModule, MatButtonModule, FormsModule, MatFormFieldModule, MatInputModule, MatGridListModule]
+})
+export class NavbarComponet{
+    cartLength: number = 0
+
+    constructor(private store: Store,){
+        this.store.select(selectCart).subscribe(
+            cart => this.cartLength = cart.length
+        )
+    }
+
+}
